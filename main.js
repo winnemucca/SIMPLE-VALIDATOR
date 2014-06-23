114095958eb2a7862a7f915bbd08872be077f68b
Steve Kauyedauty <skauyedauty@gmail.com>
2014-06-23 17:14:10 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,52 @@
+// var phone = 
+// 	prompt("Please enter your phone# use dashes to seperate Ex. 775-530-8393.  Not a span website" );
+
+// 	if(phone.charAt(3) === '-' && phone.length ===12){
+// 		alert("Thank you");
+// 	}
+// 	else {
+// 		alert("please follow the directions");
+// 	}
+
+
+// 	var birthdate = 
+// 	prompt("Please enter your birthdate. Ex: 09/02/1980.  Not a span website" );
+
+// 	if(birthdate.charAt(2) === '/' && birthdate.length ===10){
+// 		alert("Thank you");
+// 	}
+// 	else {
+// 		alert("Recall the dashes");
+// 	}
+
+
+	// var postalCode = 
+	// prompt("Please enter your complete mailing zip code Ex. 80203.  Not a span website" );
+
+	// if(postalCode.length === 5){
+	// 	alert("Thank you");
+	// }
+	// else {
+	// 	alert("please follow the directions");
+	// }
+
+	// var stateAbbrev = 
+	// prompt("Please enter your state abbreviation Ex. CO.  Please use all caps");
+
+	// if(stateAbbrev.length === 2 && stateAbbrev === stateAbbrev.toUpperCase())  {
+	// 	alert("Thank you");
+	// }
+	// else {
+	// 	alert("please check caps and length");
+	// }
+
+
+	var married = 
+		prompt("Are you married?  Identify your marital status:  yes for married or no for single");
+
+		if( married.toLowerCase() === "yes" || married.toLowerCase() === "no")  {
+		alert("Thank you");
+	}
+		else {
+		alert("please respond");
+	}
